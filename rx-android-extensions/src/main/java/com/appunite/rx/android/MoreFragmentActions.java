985503cc4f060526ba6ac6f4373a805c6744a45a
Pawel Schmidt <pawel.schmidt@appunite.com>
2015-10-16 18:11:26 +0200

    Removed finishActivity and changed popBackStack

--- rx-android-extensions/src/main/java/com/appunite/rx/android/MoreFragmentActions.java
+++ rx-android-extensions/src/main/java/com/appunite/rx/android/MoreFragmentActions.java
@@ -1,6 +1,6 @@
 package com.appunite.rx.android;
 
-import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 
 import javax.annotation.Nonnull;
 
@@ -9,21 +9,11 @@ import rx.functions.Action1;
 public class MoreFragmentActions {
 
     @Nonnull
-    public static Action1<? super Object> finishActivity(@Nonnull final Fragment fragment) {
+    public static Action1<? super Object> popBackStack(@Nonnull final FragmentManager fragmentManager) {
         return new Action1<Object>() {
             @Override
             public void call(Object o) {
-                fragment.getActivity().finish();
-            }
-        };
-    }
-
-    @Nonnull
-    public static Action1<? super Object> popBackStack(@Nonnull final Fragment fragment) {
-        return new Action1<Object>() {
-            @Override
-            public void call(Object o) {
-                fragment.getFragmentManager().popBackStack();
+                fragmentManager.popBackStack();
             }
         };
     }
