2c4a48dfdc73cf2b0122420d821f47e7bd062f69
Pawel Schmidt <pawel.schmidt@appunite.com>
2015-11-18 13:27:13 +0100

    Added show toast action

--- rx-android-extensions/src/main/java/com/appunite/rx/android/widget/RxContextMore.java
+++ rx-android-extensions/src/main/java/com/appunite/rx/android/widget/RxContextMore.java
@@ -2,10 +2,14 @@ package com.appunite.rx.android.widget;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.StringRes;
+import android.widget.Toast;
 
 import javax.annotation.Nonnull;
 
 import rx.functions.Action1;
+
+import static com.appunite.rx.internal.Preconditions.checkNotNull;
 
 public class RxContextMore {
 
@@ -14,8 +18,33 @@ public class RxContextMore {
                                                         @Nonnull final Intent intent) {
         return new Action1<Object>() {
             @Override
-            public void call(Object o) {
+            public void call(Object ignore) {
                 context.startActivity(intent);
+            }
+        };
+    }
+
+    @Nonnull
+    public static Action1<? super Object> showToast(@Nonnull final Context context,
+                                                    @Nonnull final String text) {
+        checkNotNull(context);
+        checkNotNull(text);
+        return new Action1<Object>() {
+            @Override
+            public void call(Object ignore) {
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    @Nonnull
+    public static Action1<? super Object> showToast(@Nonnull final Context context,
+                                                    @StringRes final int resId) {
+        checkNotNull(context);
+        return new Action1<Object>() {
+            @Override
+            public void call(Object ignore) {
+                Toast.makeText(context, resId, Toast.LENGTH_SHORT).show();
             }
         };
     }
